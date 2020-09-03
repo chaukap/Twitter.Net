5c66bda8b7b975e96b2090efb1b25ba57f46e658
Chandler Haukap <18161@global.ul.com>
2020-09-03 13:06:35 -0600

    Created a custom stream reader for twitter streams

---
+++ Twitter.NET/Stream/TwitterStream.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using System.Text.Json;
+
+namespace Twitter.Net.Stream
+{
+    class TwitterStream : StreamReader
+    {
+        public TwitterStream(System.IO.Stream stream) : base(stream) { }
+
+        public Tweet NextTweet()
+        {
+            return JsonSerializer.Deserialize<Tweet>(
+                ReadLine(),
+                new JsonSerializerOptions
+                {
+                    IgnoreNullValues = true,
+                    PropertyNameCaseInsensitive = true
+                });
+        }
+    }
+}
